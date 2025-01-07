fb59999fb05deaa89f1a8a4141bd991da41ccb34
Gustavo Lemos <gsvieira08@gmail.com>
2025-01-07 15:30:28 -0300

    Footer atualizado

--- inrise/src/components/AppFooter.vue
+++ inrise/src/components/AppFooter.vue
@@ -1,29 +1,96 @@
 <template>
-  <div class="footer">
-    <div class="flex-container">
-      <div class="flex-item main-column">
-        <p class = "topText" >Endereço</p>
-        <p class = "bottomText" >Rua de Sei lá da onde, nº 12983</p>
-      </div>
-      <div class="flex-item">
-        <p class = "topText" >Redes Sociais</p>
-        <div class = "iconHolder" >
-          <div class = "square-grey"></div>
-          <div class = "square-grey"></div>
-          <div class = "square-grey"></div>
-          <div class = "square-grey"></div>
+  <div>
+    <div class="w-full min-h-[157px] bg-[#191919] flex flex-col items-center mt-auto">
+      <div class="flex w-full h-full">
+        <!-- Left Section @TODO: Responsividade pra 1366x768 nessa pagina ou é na clienthome n sei -->
+        <div class="flex-grow min-w-[12rem]"></div>
+
+        <!-- Main Column (Endereço) -->
+        <div class="flex-grow flex flex-col basis-1/3 items-start text-center p-4 text-white">
+          <div class="flex flex-col items-start border-b-2 pb-4 border-white">
+            <div>
+              <img src="@/assets/InRise logo white.png" alt="Logo">
+            </div>
+            <p class="text-left text-[14px] py-[10px] text-[#B7B7B7]">A InRise Computadores transforma a busca pelo
+              computador perfeito,
+              ao oferecer mais do que tecnologia: um movimento pela democratização da informação técnica e acesso
+              a infinitas possibilidades. Com uma equipa de especialistas apaixonados por tecnologia e gamers dedicados,
+              ajudamos a encontrar o computador dos teus sonhos.
+            </p>
+            <p><i class="fas fa-phone-alt"></i> +351 962 597 198</p>
+
+            <p><i class="fas fa-envelope"></i> sac@inrise.com</p>
+          </div>
+          <!-- Payment Icons Section -->
+          <div class="payment-icons my-2 flex space-x-4">
+            <img src="@/assets/payment/pay1.png">
+            <img src="@/assets/payment/pay2.png">
+            <img src="@/assets/payment/pay3.png">
+            <img src="@/assets/payment/pay4.png">
+            <img src="@/assets/payment/pay5.png">
+            <img src="@/assets/payment/pay6.png">
+            <img src="@/assets/payment/pay7.png">
+          </div>
         </div>
 
-      </div>
-      <div class="flex-item">
-        <p class = "topText" >Fale Conosco</p>
-        <p class = "bottomText" >sac@inrise.com</p>
-        <p class = "bottomText" >+351 962 597 198</p>
-      </div>
-      <div class="flex-item">
-        <p class = "topText" >Pague Usando</p>     
+        <!-- Right Section -->
+        <div class="flex-grow min-w-[12rem]"></div>
+
+        <!-- Nos conheça @TODO: Links pra o faq, serviços, etc -->
+        <div class="text-center mx-4 text-white">
+          <p class="text-[13px] font-semibold pt-[10px] pb-[5px] border-b-[1px] border-b-white mb-[10px]">Nos conheça
+          </p>
+          <div class="flex flex-col items-start justify-center text-[10px] space-y-2">
+            <div class="">
+              FAQ
+            </div>
+            <div class="">
+              Serviços
+            </div>
+            <div class="">
+              Contactos
+            </div>
+            <div class="">
+              Quem somos
+            </div>
+          </div>
+        </div>
+
+        <!-- Fale Conosco Section -->
+        <div class="text-center mx-4 text-white">
+          <p class="text-[13px] font-semibold pt-[10px] pb-[5px] border-b-[1px] border-b-white mb-[10px]">Mais
+            Informações
+          </p>
+          <div class="flex flex-col items-start justify-center text-[10px] space-y-2">
+            <div class="">
+              RGPD
+            </div>
+            <div class="">
+              Aviso Legal
+            </div>
+            <div class="">
+              Termos do Site
+            </div>
+            <div class="">
+              Política de Cookies
+            </div>
+            <div class="">
+              Termos e Condições
+            </div>
+            <div class="">
+              Política de Privacidade
+            </div>
+            <div class="">
+              Devoluções e Garantias
+            </div>
+          </div>
+        </div>
+
+        <!-- Right Section -->
+        <div class="flex-grow min-w-[12rem]"></div>
       </div>
     </div>
+    <div class="w-full min-h-[56px] bg-[#b7b7b7] flex flex-col items-center mt-auto"></div>
   </div>
 </template>
 
@@ -32,55 +99,3 @@ export default {
   name: 'AppFooter'
 }
 </script>
-
-<style scoped>.footer {
-  width: 100%;
-  height: 157px;  /* You can adjust this as needed */
-  background: #191919;
-  padding: 0;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: auto;
-}
-
-.flex-container {
-  width: 100%;
-  height: 100%;
-  display: flex;
-}
-
-.topText {
-  padding: 10px;
-  font-size: 16px;
-  font-weight: 900;
-}
-
-.iconHolder {
-  display: flex;
-  flex-direction: row;
-}
-
-.flex-item {
-  flex-grow: 1;
-  text-align: center;
-  color: #FFF;
-}
-
-p {
-  margin: 0;
-}
-
-.main-column {
-  flex-basis: 70%;
-  
-}
-
-.square-grey {
-  flex-grow: 1;
-  width: 30px;
-  height: 30px;
-}
-
-</style>
