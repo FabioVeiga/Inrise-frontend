71fc415a7c818f6b7612565ec91b3b4a3bc18986
Gustavo Lemos <gsvieira08@gmail.com>
2025-01-21 18:32:39 -0300

    Adicao de mobo e modal preto

--- inrise/src/screens/ClientPCPartPicker.vue
+++ inrise/src/screens/ClientPCPartPicker.vue
@@ -6,47 +6,72 @@
       <p class="whitespace-nowrap">SELECIONE <br> OS HARDWARES</p>
     </HeaderRectanglesLarge>
     <AppBreadcrumbs></AppBreadcrumbs>
-    <div v-if="loading" class="text-center text-xl text-gray-500">
-      <p>Carregando peças...</p>
-    </div>
 
-    <div v-if="!loading">
-      <form @submit.prevent="submitForm" class="space-y-6">
-
-        <!-- CPU -->
-        <p style="white-space: nowrap" class="text-xl font-semibold">
-          Processadores
-        </p>
-        <PcPartRow partType="CPU" :parts="processadores" :selectedParts="[selectedParts.processador]"
-          @update:selectedParts="selectPart('processador', $event)" />
-
-        <!-- RAM -->
-        <p style="white-space: nowrap" class="text-xl font-semibold">
-          Memórias RAM
-        </p>
-        <PcPartRow partType="RAM" :parts="memoriasRam" :selectedParts="[selectedParts.memoryRam]"
-          @update:selectedParts="selectPart('memoryRam', $event)" />
-
-        <!-- Preço Final  -->
-        <div class="text-lg font-semibold">
-          <p>Preço Final: {{ finalPrice }}</p>
+    <div class="container">
+      <div class="content">
+        <div v-if="loading" class="text-center text-xl text-gray-500">
+          <p>Carregando peças...</p>
         </div>
 
-        <!-- Submit -->
-        <button type="submit" class="btn btn-primary">Salvar PC</button>
-      </form>
+        <div v-if="!loading">
+          <form @submit.prevent="submitForm" class="space-y-6">
+            <!-- CPU -->
+            <p style="white-space: nowrap" class="text-xl font-semibold">
+              Processadores
+            </p>
+            <PcPartRow partType="CPU" :parts="processadores" :selectedParts="[selectedParts.processador]"
+              @update:selectedParts="selectPart('processador', $event)" />
+
+
+            <!-- Mobo -->
+            <p style="white-space: nowrap" class="text-xl font-semibold">
+              Placas Mãe
+            </p>
+            <PcPartRow partType="MOBO" :parts="placasMae" :selectedParts="[selectedParts.mobo]"
+              @update:selectedParts="selectPart('mobo', $event)" />
+
+
+            <!-- RAM -->
+            <p style="white-space: nowrap" class="text-xl font-semibold">
+              Memórias RAM
+            </p>
+            <PcPartRow partType="RAM" :parts="memoriasRam" :selectedParts="[selectedParts.memoryRam]"
+              @update:selectedParts="selectPart('memoryRam', $event)" />
+
+
+          </form>
+        </div>
+
+
+      </div>
+
+      <!--  Modal -->
+      <div class="modal flex flex-col justify-between">
+        <div class="h-3/4">
+          <p>Modal com as peças.</p>
+        </div>
+        <!-- Preço Final  -->
+
+        <div class="h-1/4 flex w-full flex-row justify-between">
+          <div class="text-lg font-semibold">
+            <p>Preço Final: {{ finalPrice }}</p>
+          </div>
+
+          <!-- Submit -->
+          <ActionButton :to="{ name: 'ClientPCPartPicker' }" :isNext="true" :isFinish="true">
+            Finalizar Pagamento
+          </ActionButton>
+        </div>
+      </div>
     </div>
     <div class="flex justify-between w-full max-w-[1366px]">
-      <ActionButton :to="{ name: 'ClientActivity' }" :isNext="false">
+      <ActionButton :to="{ name: 'ClientSoftware' }" :isNext="false">
         Página Anterior
-      </ActionButton>
-
-      <ActionButton :to="{ name: 'ClientPCPartPicker' }" :isNext="true">
-        Próxima Página
       </ActionButton>
     </div>
   </HomeContentView>
 </template>
+
 
 <script>
 import PcPartRow from '@/components/PcPartRow.vue';
@@ -69,9 +94,18 @@ export default {
       selectedParts: {
         processador: null,
         memoryRam: null,
+        mobo: null,
+
       },
       processadores: [],
+      placasMae: [],
+      gabinetes: [],
       memoriasRam: [],
+      discos: [],
+      placasVideo: [],
+      fontesAlimentacao: [],
+      coolers: [],
+      monitores: [],
       finalPrice: 0
     };
   },
@@ -79,24 +113,38 @@ export default {
     async fetchData() {
       this.loading = true;
       try {
-        const [processadores, memoriasRam] = await Promise.all([
+        const [processadores, placasMae, gabinetes, memoriasRam, discos, placasVideo, fontesAlimentacao, coolers, monitores] = await Promise.all([
           loadProducts('processador'),
+          loadProducts('placaMae'),
+          loadProducts('gabinete'),
           loadProducts('ram'),
+          loadProducts('disco'),
+          loadProducts('placaDeVideo'),
+          loadProducts('psu'),
+          loadProducts('cooler'),
+          loadProducts('monitor'),
         ]);
-
-        this.processadores = processadores;
-        this.memoriasRam = memoriasRam;
         this.loading = false;
+        this.processadores = processadores;
+        this.placasMae = placasMae;
+        this.gabinetes = gabinetes;
+        this.memoriasRam = memoriasRam;
+        this.discos = discos;
+        this.placasVideo = placasVideo;
+        this.fontesAlimentacao = fontesAlimentacao;
+        this.coolers = coolers;
+        this.monitores = monitores;
+        console.log("Thispla", this.placasMae)
       } catch (error) {
         console.error('Erro ao carregar dados:', error);
-        this.loading = false;
       }
+
     },
     selectPart(partType, updatedPart) {
-      console.log(`Selected part in ${partType}:`, updatedPart); 
+      console.log(`Selected part in ${partType}:`, updatedPart);
 
       this.selectedParts[partType] = updatedPart;
-      console.log('Updated selectedParts:', this.selectedParts); 
+      console.log('Updated selectedParts:', this.selectedParts);
 
       //this.calculateFinalPrice();
       Cookies.set('selectedPcParts', JSON.stringify(this.selectedParts), { path: '/' });
@@ -122,3 +170,29 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  height: 100vh;
+}
+
+.content {
+  width: 60%;
+  overflow-y: auto;
+  padding: 20px;
+}
+
+.modal {
+  width: 40%;
+  background-color: black;
+  color: white;
+  position: sticky;
+  top: 0;
+  height: 100vh;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
