d8d8f8e545a99873c1fe63eb6c72113073a11f90
Gustavo Lemos <gsvieira08@gmail.com>
2025-02-25 19:31:45 -0300

    feature: carousel passing one at a time

--- inrise/src/components/PcPartRow.vue
+++ inrise/src/components/PcPartRow.vue
@@ -7,11 +7,13 @@
         </div>
 
         <div class="flex justify-between mt-4">
-            <button type="button" class="bg-blue-500 text-white px-4 py-2 rounded" @click="prevSlide">
-                Previous
+            <button type="button" class="bg-blue-500 text-white px-4 py-2 rounded" @click="prevSlide"
+                >
+                Anterior
             </button>
-            <button type="button" class="bg-blue-500 text-white px-4 py-2 rounded" @click="nextSlide">
-                Next
+            <button type="button" class="bg-blue-500 text-white px-4 py-2 rounded" @click="nextSlide"
+                >
+                Próximo
             </button>
         </div>
     </div>
@@ -21,62 +23,66 @@
 import PcPart from './PcPart.vue';
 
 export default {
-  name: 'PcPartRow',
-  components: {
-    PcPart,
-  },
-  props: {
-    partType: {
-      type: String,
-      required: true,
+    name: 'PcPartRow',
+    components: {
+        PcPart,
     },
-    parts: {
-      type: Array,
-      required: true,
+    props: {
+        partType: {
+            type: String,
+            required: true,
+        },
+        parts: {
+            type: Array,
+            required: true,
+        },
+        selectedParts: {
+            type: Array,
+            required: true,
+        },
     },
-    selectedParts: {
-      type: Array,
-      required: true,
+    data() {
+        return {
+            localSelectedPart: this.selectedParts[0] || null,
+            currentIndex: 0, 
+        };
     },
-  },
-  data() {
-    return {
-      localSelectedPart: this.selectedParts[0] || null,
-      currentIndex: 0, 
-    };
-  },
-  computed: {
-    currentParts() {
-      return this.parts.slice(this.currentIndex, this.currentIndex + 3);
+    computed: {
+        currentParts() {
+            return this.parts.slice(this.currentIndex, this.currentIndex + 3);
+        },
     },
-  },
-  methods: {
-    updateSelectedPart(updatedPart) {
-      this.localSelectedPart = updatedPart;
-      this.$emit('update:selectedParts', updatedPart);
+    methods: {
+        updateSelectedPart(updatedPart) {
+            this.localSelectedPart = updatedPart;
+            this.$emit('update:selectedParts', updatedPart);
+        },
+        prevSlide() {
+            if (this.currentIndex === 0) {
+                this.currentIndex = this.parts.length - 3;
+            } else {
+                this.currentIndex -= 1;
+            }
+        },
+        nextSlide() {
+            if (this.currentIndex + 3 >= this.parts.length) {
+                this.currentIndex = 0;
+            } else {
+                this.currentIndex += 1;
+            }
+        },
     },
-    prevSlide() {
-      if (this.currentIndex > 0) {
-        this.currentIndex -= 3; 
-      }
+    watch: {
+        selectedParts(newVal) {
+            this.localSelectedPart = newVal[0] || null;
+        },
     },
-    nextSlide() {
-      if (this.currentIndex + 3 < this.parts.length) {
-        this.currentIndex += 3; 
-      }
-    },
-  },
-  watch: {
-    selectedParts(newVal) {
-      this.localSelectedPart = newVal[0] || null;
-    },
-  },
 };
 </script>
 
 <style scoped>
 button:disabled {
-  background-color: #ccc;
-  cursor: not-allowed;
+    background-color: #ccc;
+    cursor: not-allowed;
 }
 </style>
