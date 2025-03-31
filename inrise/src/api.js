43d0f00eaa8a166dc5dc3976e0cc2252b8e77b40
Gustavo Lemos <gsvieira08@gmail.com>
2025-03-31 16:16:34 -0300

    fix: api key

--- inrise/src/api.js
+++ inrise/src/api.js
@@ -17,7 +17,7 @@ const getUserToken = () => {
 };
 
 const apiClient = axios.create({
-  baseURL: 'https://apiinriseservice.azurewebsites.net/',
+  baseURL: 'https://api-inrise-service.azurewebsites.net/',
   headers: {
     'Content-Type': 'application/json',
     'secret': 'naf9uafjh_+mcdsaIFD023',
